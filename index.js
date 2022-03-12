5d9faea41279a31afa678f617309cb0bed6256d6
maxarvid <max.arvid.anderson@gmail.com>
2022-03-12 14:29:29 +0100

    removes traceroute and inquirer, adds traceroute-lite

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 #!/usr/bin/env node
+
+import traceroute from "traceroute-lite"
+
+var tr = new traceroute('google.com');
+
+tr.on('hop', function(hop) {
+  console.log('Add API IP to Flag emoji call here')
+  hop['country'] = "Sweden"
+  console.log(hop); // { counter: 1, ip: '1.2.3.4', ms: 12 }
+});
+
+tr.start();
